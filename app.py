392b1988d758dc5d4c883338d54af1943fbda8f3
NaveenNV2303 <138974584+NaveenNV2303@users.noreply.github.com>
2023-12-01 12:36:53 +0000

    commit 1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def testConnection():
- return("Connection is established successfully")
+ return("Connection is established successfully on dev")
   
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port='8080', ssl_context=('cert.pem', 'privkey.pem'))
